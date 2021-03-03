a42cf24a466a3600409d781a5999136ba9deb2da
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2021-03-03 10:30:53 -0600

    firs tcommit

---
+++ src/main/java/com/tts/swaggerpractice/SwaggerpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.tts.swaggerpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerpracticeApplication.class, args);
+	}
+
+}
