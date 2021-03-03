ed1529b6c275d59d8ec68def9e05edcbb9b7ef55
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2021-03-03 13:00:09 -0600

    v2 commit

--- src/main/java/com/tts/swaggerpractice/config/SwaggerConfig.java
+++ src/main/java/com/tts/swaggerpractice/config/SwaggerConfig.java
@@ -9,6 +9,9 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -16,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.tts.swaggerpractice"))
@@ -25,14 +28,22 @@ public class SwaggerConfig {
                 .apiInfo(buildApiInfo());
     }
 
-    private ApiInfo buildApiInfo(){
+    private ApiInfo buildApiInfo() {
         return new ApiInfoBuilder()
                 .title("Menu Items API")
-                .description("Rest API for interacting with menu items")
+                .description("REST API for interacting with menu items")
                 .version("1.0.0")
-                .contact(new Contact("Developer name", "website.com", "developer@website.com"))
+                .contact(new Contact("Monsieur Johnson", "Swagger.com", "MonsieurJohnson@swagger.com"))
                 .license("Apache License Version 2.0")
                 .license("https://www.apache.org/licenses/LICENSE-2.0")
                 .build();
     }
+
+    @Bean
+    public UiConfiguration buildUIConfig() {
+        return UiConfigurationBuilder.builder()
+                .docExpansion(DocExpansion.FULL)
+                .build();
+    }
+
 }
